bcfe1657640f2d557c3d7fc9e0018dfcb387279c
ultomnis <125839032+ultomnis@users.noreply.github.com>
2025-10-02 17:07:59 -0400

    Remove linkedin

--- _data/meta.mjs
+++ _data/meta.mjs
@@ -2,7 +2,6 @@ export default {
   email: "contact@jmsyu.com",
   github: "https://github.com/ultomnis/",
   license: "https://github.com/ultomnis/personal-website/blob/main/LICENSE",
-  linkedin: "https://linkedin.com/in/jmsyu",
   name: "James Yu",
 
   rss: {
